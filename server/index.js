9b6cbc1eb7766a1a359123085485f6f85c582e05
Matthew Faw <matthewfaw@gmail.com>
2017-01-05 20:44:24 -0500

    sets up post requests to server

--- server/index.js
+++ server/index.js
@@ -1,13 +1,27 @@
 import express from 'express';
 import path from 'path';
+import bodyParser from 'body-parser';
 
 const app = express();
-const PORT = 8080;
+const PORT = 3000;
 
 app.use(express.static(path.resolve(__dirname, '..', 'build')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.all('/*', function(req,res,next) {
+    res.header("Access-Control-Allow-Origin","*");
+    res.header("Access-Control-Allow-Headers","X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods","POST, GET, DELETE");
+    next();
+});
 
 app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, '..', 'build', 'index.html'));
+});
+
+app.post('/list', (req, res) => {
+    console.log(req.body)
 });
 
 app.listen(PORT, function () {
--- src/actions/asyncActions.js
+++ src/actions/asyncActions.js
@@ -1,6 +1,7 @@
 import { submitTerminalInputText } from './actions';
 import fetch from 'isomorphic-fetch'
 import ActionStatus from './actionStatus';
+import { SERVER_URL } from '../utils/defaults';
 
 export const fetchSongs = (artist) => {
     return dispatch => {
@@ -9,5 +10,16 @@ export const fetchSongs = (artist) => {
         return fetch(`https://api.spotify.com/v1/search?q=${stripped}&type=artist`)
             .then(response => response.json())
             .then(json => dispatch(submitTerminalInputText(artist, json.artists.items.map(artist => artist.name), ActionStatus.FINISHED)))
+            .then(fetch(`${SERVER_URL}/list`, {
+                headers: {
+                    'Accept':'text/plain',
+                    'Content-Type': 'application/json',
+                },
+                method: 'post',
+                body: JSON.stringify({
+                    name: 'Hubot',
+                    login: artist,
+                })
+            }))
     };
 }
